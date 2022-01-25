ba20fa0478857e9a2c2b99ca29f7df640c1587a1
Mejanur Rahman Akash <43064536+mrmezan06@users.noreply.github.com>
2022-01-25 16:48:58 +0600

    PODO fixed with flutter documentation

--- json_parsing/lib/json_parsing_map.dart
+++ json_parsing/lib/json_parsing_map.dart
@@ -1,108 +1,110 @@
 import 'dart:convert';
-import 'package:http/http.dart';
 import 'package:flutter/material.dart';
-import 'package:json_parsing/model/post.dart';
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
-class JsonParsingMap extends StatefulWidget {
-  const JsonParsingMap({Key? key}) : super(key: key);
+Future<List<Post>> fetchPosts(http.Client client) async {
+  final response = await client
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
 
-  @override
-  _JsonParsingMapState createState() => _JsonParsingMapState();
+  // Use the compute function to run parsePosts in a separate isolate.
+  return compute(parsePosts, response.body);
 }
 
-class _JsonParsingMapState extends State<JsonParsingMap> {
-  late Future<PostList> data;
+// A function that converts a response body into a List<Post>.
+List<Post> parsePosts(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed.map<Post>((json) => Post.fromJson(json)).toList();
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  const Post({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'] as int,
+      id: json['id'] as int,
+      title: json['title'] as String,
+      body: json['body'] as String,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    data = getData();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: FutureBuilder<List<Post>>(
+        future: fetchPosts(http.Client()),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('An error has occurred!'),
+            );
+          } else if (snapshot.hasData) {
+            return PostsList(data: snapshot.data!);
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
   }
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("PODO"),
-        ),
-        body: Center(
-          child: Container(
-            child: FutureBuilder(
-              future: getData(),
-              builder: (context, AsyncSnapshot snapshot) {
-                if(snapshot.hasData){
-                  print("Data ready");
-                  return createListView(snapshot.data!.posts, context);
-                } else{
-                  print("${snapshot.toString()}");
-                  return CircularProgressIndicator();
-                }
+}
 
-              },
-            ),
-          ),
-        ),
-      );
-    }
+class PostsList extends StatelessWidget {
+  const PostsList({Key? key, required this.data}) : super(key: key);
 
-  Future<PostList> getData() async {
-    // String url = "https://jsonplaceholder.typicode.com/posts";
-    // var url = Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{http}'});
-    var url =
-    Uri.https('jsonplaceholder.typicode.com', '/posts',);
-    return Network(url).loadPosts();
-    // data.then((value){
-    //   print(value[0]['title']);
-    // });
-   // print(data.toString());
-   // return data;
+  final List<Post> data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Divider(height: 5.0),
+              ListTile(
+                title: Text(data[index].title),
+                subtitle: Text(data[index].body),
+                leading: Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.black26,
+                      radius: 23.0,
+                      child: Text("${data[index].id}"),
+                    )
+                  ],
+                ),
+              )
+            ],
+          );
+        },
+      ),
+    );
   }
-
-    Widget createListView(List<Post> data, BuildContext context) {
-      //print(data[0].title);
-      return Container(
-        child: ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context, int index) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Divider(height: 5.0),
-                  ListTile(
-                    title: Text("${data[index].title}"),
-                    subtitle: Text("${data[index].body}"),
-                    leading: Column(
-                      children: [
-                        CircleAvatar(
-                          backgroundColor: Colors.black26,
-                          radius: 23,
-                          child: Text("${data[index].id}"),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              );
-            }),
-      );
-    }
-  }
-
-
-
-class Network{
-  final Uri url;
-
-  Network(this.url);
-  Future<PostList> loadPosts() async {
-    final response = await get(url);
-    if ( response.statusCode == 200 ){
-      print(response.body);
-      return PostList.fromJson(json.decode(response.body));
-    }else{
-      print("Exception to load posts");
-      throw Exception("Failed to get posts");
-    }
-  }
-
 }
--- json_parsing/lib/main.dart
+++ json_parsing/lib/main.dart
@@ -3,11 +3,28 @@ import 'package:json_parsing/json_parsing_map.dart';
 import 'package:json_parsing/parsing%20json.dart';
 
 
-void main() {
-  runApp(
-      const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: JSON_Parsing(),
-      )
-  );
+// void main() {
+//   runApp(
+//       const MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home: JsonParsingMap(),
+//       )
+//   );
+// }
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Json Parsing';
+
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
 }
