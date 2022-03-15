293df0b16d5108852b7b660356acf1444cec0061
Mejanur Rahman Akash <43064536+mrmezan06@users.noreply.github.com>
2022-03-15 22:19:33 +0600

    delete wallpaper store

--- wallpaper_store/android/app/src/main/kotlin/com/wallapaperengine/wallpaper_store/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.wallapaperengine.wallpaper_store
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- wallpaper_store/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:wallpaper_store/ui/main_ui.dart';
-
-void main() {
-  runApp(
-      const MainUI()
-  );
-}
-
