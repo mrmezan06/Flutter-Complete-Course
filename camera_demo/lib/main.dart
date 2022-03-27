0521006da8d196ef0cd7fb1e188f0cfa29d85d02
Mejanur Rahman Akash <mrmezan06@gmail.com>
2022-03-27 14:37:43 +0600

    camera access and file read write access

---
+++ camera_demo/lib/main.dart
@@ -0,0 +1,92 @@
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  final fCamera = cameras.first;
+  runApp(MaterialApp(
+    theme: ThemeData.dark(),
+    home: TakePictureScreen(camera: fCamera),
+  ));
+}
+
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({Key? key, required this.camera}) : super(key: key);
+
+  final CameraDescription camera;
+
+  @override
+  State<TakePictureScreen> createState() => _TakePictureScreenState();
+}
+
+class _TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = CameraController(widget.camera, ResolutionPreset.medium);
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Take a picture'),
+      ),
+      body: FutureBuilder(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+            final image = await _controller.takePicture();
+            await Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) =>
+                    DisplayPictureScreen(path: image.path)));
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
+
+class DisplayPictureScreen extends StatelessWidget {
+  const DisplayPictureScreen({Key? key, required this.path}) : super(key: key);
+
+  final String path;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( 
+        title: const Text('Image Preview'),
+      ),
+      body: Image.file(File(path), fit: BoxFit.cover, width: double.infinity, height: double.infinity,),
+    );
+  }
+}
---
+++ simple_file_op/android/app/src/main/kotlin/com/example/simple_file_op/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simple_file_op
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
